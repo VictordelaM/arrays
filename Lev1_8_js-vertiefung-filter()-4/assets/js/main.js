a7848d009212e8d4b8cc292a75e74328801771c6
VictordalaM <delamotte.v@gamil.com>
2023-12-18 13:34:43 +0100

    fertig nur noch bonus fehlt

--- Lev1_8_js-vertiefung-filter()-4/assets/js/main.js
+++ Lev1_8_js-vertiefung-filter()-4/assets/js/main.js
@@ -1,2 +1,15 @@
-const heroArr = ["Superman", "Batman", undefined, , "Wonder Woman", "Spider-Man", "Black Widow", "Iron Man", "Thor", "Catwoman", , null];
-
+function filter (){
+    const heroArr = ["Superman", "Batman", undefined, , "Wonder Woman", "Spider-Man", "Black Widow", "Iron Man", "Thor", "Catwoman", , null];
+    console.log(heroArr)
+    let ergebnisOhneUndefined = heroArr.filter(removeUn)
+    console.log(ergebnisOhneUndefined)
+    let ergebnisOhneNull = ergebnisOhneUndefined.filter(removeNull)
+    console.log(ergebnisOhneNull)
+    function removeUn (word){
+        return word !== undefined
+    }
+    function removeNull (word){
+        return word !== null
+    }
+}
+filter()
